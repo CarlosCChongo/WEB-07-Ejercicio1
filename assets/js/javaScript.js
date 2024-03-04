f52225afa912d0ca16500c90dae914a907a81ace
Carlos Chongo <cacc.chongo@gmail.com>
2024-03-04 00:08:01 -0600

    FIX: se corrigen paramentros del documento

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -2,7 +2,7 @@ function cambiarNombre() {
     let nombre = prompt("¿Cuál es tu nombre?");
     
     if (nombre != null) {
-        document.getElementById("saludo").innerText = `!Hola ${nombre}!, es un gusto conocerte, espero estes teniendo un bonito dia`;
+        document.getElementById("saludo").innerText = `!Hola ${nombre}!`;
     
     }
 }
